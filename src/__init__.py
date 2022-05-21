6fedbc8d63e5bd835ef4866371c6a57926577b79
Michael Mora <maycolmora99@gmail.com>
2022-05-20 23:59:37 -0500

    commit-10: adding token time jwt

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from .services.authentication.auth import auth
@@ -15,7 +16,9 @@ def create_app(test_config=None):
             SECRET_KEY=os.environ.get('SECRET_KEY'),
             SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI'),
             SQLALCHEMY_TRACK_MODIFICATIONS=False,
-            JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY")
+            JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY"),
+            JWT_ACCESS_TOKEN_EXPIRES=timedelta(days=1),
+            JWT_REFRESH_TOKEN_EXPIRES=timedelta(days=30)
         )
     else:
         app.config.from_mapping(test_config)
